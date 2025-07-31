978f3be44ec07421603ea8d5d6f881ffc8223bf5
Yemen <166258777+Sn4a5s6h@users.noreply.github.com>
2025-08-01 00:36:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'✅ Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == "السلام عليكم":
+        await message.channel.send("وعليكم السلام! كيف أقدر أساعدك؟")
+
+    elif message.content.lower() == "ping":
+        await message.channel.send("Pong!")
+
+client.run(TOKEN)
